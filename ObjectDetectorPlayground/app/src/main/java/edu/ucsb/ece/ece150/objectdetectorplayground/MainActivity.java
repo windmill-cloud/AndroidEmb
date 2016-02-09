7ab37308cb3a323171f9a604a38a5e4598efdc0a
keyrrae <xuan-wang@outlook.com>
2016-02-09 08:48:51 -0800

    add classifier, with issue

--- ObjectDetectorPlayground/app/src/main/java/edu/ucsb/ece/ece150/objectdetectorplayground/MainActivity.java
+++ ObjectDetectorPlayground/app/src/main/java/edu/ucsb/ece/ece150/objectdetectorplayground/MainActivity.java
@@ -1,6 +1,8 @@
 package edu.ucsb.ece.ece150.objectdetectorplayground;
 
 import android.app.Activity;
+import android.content.Context;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,14 +15,31 @@ import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
 import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 
 public class MainActivity extends Activity implements CvCameraViewListener2 {
 
     private CameraBridgeViewBase mOpenCvCameraView;
     public String TAG = "OpenCV";
+    public File mCascadeFile;
+    public CascadeClassifier haarCascade=null;
+    public CascadeClassifierBase
+    //private CvHaarClassifierCascade cascadeClassifier = null;
+    private Mat mRgba;
+    private String filename = "cascade.xml";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,8 +64,56 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
                 case LoaderCallbackInterface.SUCCESS:
                 {
                     Log.i(TAG, "OpenCV loaded successfully");
+                   try{
+
+
+                        //InputStream is = getResources().openRawResource(R.raw.haarcascade_frontalface_default);
+                        InputStream is = getResources().getAssets().open("haarcascade_frontalface_default_iso.xml");
+                        //InputStream is = assetManager.open("haarcascade_frontalface_default.xml");
+
+
+                        if (is == null) {
+                            Log.e("Cascade Error", "input stream null");
+                        }
+                        File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
+
+                        mCascadeFile = new File(cascadeDir,"cascade.xml");
+                        FileOutputStream os = new FileOutputStream(mCascadeFile);
+
+                        //FileOutputStream os = new FileOutputStream(file);
+
+
+                        byte[] buffer = new byte[4096];
+
+                        int bytesRead;
+                        while((bytesRead = is.read(buffer)) != -1) {
+                            Log.i("content", buffer.toString());
+                            os.write(buffer, 0, bytesRead);
+
+                        }
+                        is.close();
+                        os.close();
+                        //haarCascade = new CascadeClassifier(mCascadeFile.getAbsolutePath());
+                        haarCascade = new CascadeClassifier("/mnt/sdcard/test/haarcascade_frontalface_default.xml");
+
+                       if (haarCascade.empty())
+                       {
+                           Log.i("Cascade Error","Failed to load cascade classifier");
+                           //haarCascade = null;
+                       }
+
+
+
+
+                    }
+                    catch(Exception e) {
+                        Log.e("Cascade Error: ", "Cascade not found");
+                        Log.e("Cascade Error: ", e.toString());
+
+                    }
                     mOpenCvCameraView.enableView();
-                } break;
+               }
+                    break;
                 default:
                 {
                     super.onManagerConnected(status);
@@ -59,7 +126,7 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
     public void onResume()
     {
         super.onResume();
-        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_6, this, mLoaderCallback);
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_1_0, this, mLoaderCallback);
     }
 
     @Override
@@ -77,13 +144,50 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
     }
 
     public void onCameraViewStarted(int width, int height) {
+        mRgba = new Mat(height, width, CvType.CV_8UC4);
     }
 
     public void onCameraViewStopped() {
     }
 
-    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+    protected void writeFile(){
+        String string = "";
+        Log.i("Write a File", filename);
+        FileOutputStream outputStream;
 
-        return inputFrame.rgba();
+        try {
+            outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+            outputStream.write(string.getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        Mat mGray = inputFrame.gray();
+        mRgba = inputFrame.rgba();
+        /*if (mIsFrontCamera)
+        {
+            Core.flip(mRgba, mRgba, 1);
+        }*/
+
+        //Detecting face in the frame
+        MatOfRect faces = new MatOfRect();
+        if(haarCascade != null)
+        {
+            Log.i("haha","haha");
+            haarCascade.detectMultiScale(mGray, faces, 1.1, 2, 2, new Size(200,200), new Size());
+        }
+        Rect[] facesArray = faces.toArray();
+        Log.i("NumOfFaces",Integer.toString(facesArray.length));
+        for (int i = 0; i < facesArray.length; i++)
+            Imgproc.rectangle(mRgba, facesArray[i].tl(), facesArray[i].br(), new Scalar(100), 3);
+        return mRgba;
+
+
+
+        //return inputFrame.rgba();
     }
 }
